edcf8a03d8e0e4ca70723dd636ec086791c3a040
zw1st <144824777+zw1st@users.noreply.github.com>
2025-03-28 14:33:00 +0400

    Готовые модельки

---
+++ IvanSusaninProject_Contracts/DataModels/GuarantorDataModel.cs
@@ -0,0 +1,13 @@
+﻿
+namespace IvanSusaninProject_Contracts.DataModels;
+
+public class GuarantorDataModel(string id, string login, string password, string email)
+{
+    public string Id { get; private set; } = id;
+
+    public string Login { get; private set; } = login;
+
+    public string Password { get; private set; } = password;
+
+    public string Email { get; private set; } = email;
+}
---
+++ IvanSusaninProject_Contracts/DataModels/PlaceDataModel.cs
@@ -0,0 +1,17 @@
+﻿
+namespace IvanSusaninProject_Contracts.DataModels;
+
+public class PlaceDataModel(string id, string address, string city, string name, string groupId, string guaranderId)
+{
+    public string Id { get; private set; } = id;
+
+    public string Address { get; private set; } = address;
+
+    public string City { get; private set; } = city;
+
+    public string Name { get; private set; } = name;
+
+    public string GroupId { get; private set; } = groupId;
+
+    public string GuaranderId { get; private set; } = guaranderId;
+}
