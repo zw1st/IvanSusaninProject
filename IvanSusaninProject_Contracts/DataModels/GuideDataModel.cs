d0f1de5276b2332f8ba77d21f6f674f914a6caf2
145Game <ivanovdenis98040@gmail.com>
2025-03-28 14:23:34 +0400

    Классы для контрактов роли поручителя

---
+++ IvanSusaninProject_Contracts/DataModels/GuideDataModel.cs
@@ -0,0 +1,15 @@
+﻿
+namespace IvanSusaninProject_Contracts.DataModels;
+
+public class GuideDataModel(string id, string fio, int experience, int age, string guaranderId)
+{
+    public string Id { get; private set; } = id;
+
+    public string Fio { get; private set;} = fio;
+
+    public int Experience { get; private set;} = experience;
+
+    public int Age { get; private set;} = age;
+
+    public string GuaranderId { get; private set;} = guaranderId;
+}
---
+++ IvanSusaninProject_Contracts/DataModels/TripDataModel.cs
@@ -0,0 +1,17 @@
+﻿
+namespace IvanSusaninProject_Contracts.DataModels;
+
+public class TripDataModel(string id, string startcity, string endcity, DateTime tripDate, int duration, string guaranderId)
+{
+    public string Id { get; private set; } = id;
+
+    public string StartCity { get; private set; } = startcity;
+
+    public string EndCity { get; private set; } = endcity;
+
+    public DateTime TripDate {  get; private set; } = tripDate;
+
+    public int Duration { get; private set; } = duration;
+
+    public string GuaranderId { get; private set; } = guaranderId;
+}
