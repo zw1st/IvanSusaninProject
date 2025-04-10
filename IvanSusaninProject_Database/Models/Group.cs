112c4a59ab8e2caa8040c10acfeed5e5a6dd53bd
zw1st <144824777+zw1st@users.noreply.github.com>
2025-04-10 16:48:12 +0400

    готовые Models и DbContext

---
+++ IvanSusaninProject_Database/Models/Group.cs
@@ -0,0 +1,25 @@
+﻿using IvanSusaninProject_Contracts.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IvanSusaninProject_Database.Models;
+
+internal class Group
+{
+    public required string Id {get; set; } = Guid.NewGuid().ToString();
+
+    public int HumanAmount { get; set;}
+
+    public HumanType HumanType {  get; set;}
+
+    public required string ExecutorId { get; set;}
+
+    public Executor? Executor { get; set; }
+
+    [ForeignKey("GroupId")]
+    public List<TourGroup>? TourGroups { get; set; }
+}
---
+++ IvanSusaninProject_Database/Models/Tour.cs
@@ -0,0 +1,28 @@
+﻿using IvanSusaninProject_Contracts.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IvanSusaninProject_Database.Models;
+
+internal class Tour
+{
+    public required string Id { get; set; } = Guid.NewGuid().ToString();
+
+    public required string Name { get; set; }
+
+    public HumanType HumanType { get; set; }
+
+    public required string ExecutorId { get; set; }
+
+    public Executor? Executor { get; set; }
+
+    [ForeignKey("TourId")]
+    public List<TourGroup>? TourGroups { get; set; }
+
+    [ForeignKey("TourId")]
+    public List<TourExcursion>? TourExcursions { get; set; }
+}
