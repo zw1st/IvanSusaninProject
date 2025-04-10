222449c69bc33a46526e89ddcc02d83f181f30d7
zw1st <144824777+zw1st@users.noreply.github.com>
2025-04-10 16:55:26 +0400

    убрал юзинги

--- IvanSusaninProject_Contracts/Infrastructure/IConfigurationDatabase.cs
+++ IvanSusaninProject_Contracts/Infrastructure/IConfigurationDatabase.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace IvanSusaninProject_Contracts.Infrastructure;
+﻿namespace IvanSusaninProject_Contracts.Infrastructure;
 
 public interface IConfigurationDatabase
 {
